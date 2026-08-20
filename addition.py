bea27db1788e376f939afc7ed4cde163d99078c9
MarriNagaLakshmi <marri.nagalakshmi2024@vitstudent.ac.in>
2026-08-20 11:12:10 +0530

    update code with new user config

--- addition.py
+++ addition.py
@@ -1,6 +1,19 @@
-a = 10
-b = 20
-result = a + b
-print("First Number:", a)
-print("Second Number:", b)
-print("Sum:", result)
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+# Test Values
+num1 = 10
+num2 = 20
+
+print("--- Jenkins Test Execution ---")
+print("First Number:", num1)
+print("Second Number:", num2)
+print("Sum:", add(num1, num2))
+print("Difference:", subtract(num1, num2))
+print("Product:", multiply(num1, num2))
